6fb16e357a9067f34810a0fd7662ee1564aabce8
Arnur Kupanov <ncase01@gmail.com>
2020-12-03 14:54:57 +0600

    Remove env file

--- src/firebase.js
+++ src/firebase.js
@@ -11,14 +11,6 @@ function initFirebase() {
     storageBucket: 'car-rental-eec8c.appspot.com',
     messagingSenderId: '198736107539',
     appId: '1:198736107539:web:a615c6b39ff069687d07e6',
-
-    // apiKey: proccess.env.API_KEY,
-    // authDomain: proccess.env.AUTH_DOMAIN,
-    // databaseURL: proccess.env.DATAVASE_URL,
-    // projectId: proccess.env.PROJECT_ID,
-    // storageBucket: proccess.env.STORAGE_BUCKET,
-    // messagingSenderId: proccess.env.MESSAGE_SENDER_ID,
-    // appId: proccess.env.APP_ID,
   };
 
   try {
